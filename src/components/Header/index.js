427c34dfe9940fdb1799b4b3305bda344935c537
Chris Davis <cdav49719@.com>
2020-07-20 12:15:50 -0700

    first commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './styles.scss';
 import Logo from './../../assets/orange.png';
-
+import { auth } from './../../firebase/utils';
 import { Link } from 'react-router-dom';
 
 
 
 const Header = props => {
-
+    const { currentUser } = props;
 return (
     <header className ='header'>
         <div className='wrap'>
@@ -17,24 +17,46 @@ return (
                 </Link>
             </div>
             
-
+       
         
        <div className='callToActions'>
+
+       {currentUser && (
+            <ul>
+            <li>
+              <a onClick={() => auth.signOut()}>
+              logout</a>  
+            </li>    
+            </ul>
+        )}
+           {!currentUser && (
+           
            <ul>
             <li>
             <Link to='/registration'>
             Register
             </Link>
+        
+                </li>
+            
+            <li>
+            <Link to='/login'>
+            Login
+            </Link>
 
             </li>
 
            </ul>
-             
+           )}
         </div>
         </div>
      
     </header>
 );
+};
+
+Header.defaultProps ={
+    currentUser : null
 };
 
 export default Header;
---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const  firebaseConfig = {
+    apiKey: "AIzaSyBoInjlBfvk_0I7gWl4zk0ZwVsCMBUKwm4",
+    authDomain: "my-website-30082.firebaseapp.com",
+    databaseURL: "https://my-website-30082.firebaseio.com",
+    projectId: "my-website-30082",
+    storageBucket: "my-website-30082.appspot.com",
+    messagingSenderId: "255856019786",
+    appId: "1:255856019786:web:ff84223f6443bd8ce299b3"
+  };
